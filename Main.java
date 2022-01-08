da81121e1c460c9a7d5d2395503ff0bc202dbbd0
KOTTAGE N.V.H <95292564+NOWEN-KOTTAGE@users.noreply.github.com>
2022-01-08 08:01:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+//Fig. 2.6: Main.java
+//Displaying multiple lines with method System.out.printf.
+public class Main{
+    //main method begins execution of Java application
+    public static void main(String[] args) {
+    	System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+    }//end method main
+}//end class Main
